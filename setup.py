2069670d115082b93972eb7ab8a906a54ad742e1
chandlertee <chandler.thompson@gmail.com>
2017-05-23 20:00:51 -0500

    Add subpackages

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='Crypto',
     version='1.4.1',
-    packages=['Crypto'])
+    packages=find_packages(),
+)
